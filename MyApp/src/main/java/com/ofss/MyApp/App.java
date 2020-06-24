734a47b5998399561676027c16684b0f3eb99d00
Sagar <sagar.kijbile@gmail.com>
2020-06-24 11:25:30 +0530

    This is the first commit from Eclipse

---
+++ MyApp/src/main/java/com/ofss/MyApp/App.java
@@ -0,0 +1,16 @@
+package com.ofss.MyApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Calculator c = new Calculator();
+        System.out.println( c.add(4, 5) );
+        
+    }
+}
